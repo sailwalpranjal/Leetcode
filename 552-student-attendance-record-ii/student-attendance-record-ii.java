30f985a7a5960f5c1611c8d83f047e53aa011b20
Pranjal Sailwal <127615786+sailwalpranjal@users.noreply.github.com>
2024-05-27 03:07:43 +0530

    Time: 85 ms (80.65%) | Memory: 40 MB (98.57%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.java
+++ 552-student-attendance-record-ii/student-attendance-record-ii.java
@@ -1,27 +1,32 @@
 class Solution {
     public int checkRecord(int n) {
         final int MOD = 1000000007;
-        int[][] dpState = new int[2][3];
-        dpState[0][0] = 1;
+        int[][] dpCurrState = new int[2][3];
+        int[][] dpNextState = new int[2][3];
+        dpCurrState[0][0] = 1;
         for (int len = 0; len < n; len++) {
-            int[][] dpNextState = new int[2][3];
+            for (int i = 0; i < 2; i++) {
+                Arrays.fill(dpNextState[i], 0);
+            }
             for (int totalAbsences = 0; totalAbsences < 2; totalAbsences++) {
                 for (int consecutiveLates = 0; consecutiveLates < 3; consecutiveLates++) {
-                    dpNextState[totalAbsences][0] = (dpNextState[totalAbsences][0] + dpState[totalAbsences][consecutiveLates]) % MOD;
+                    dpNextState[totalAbsences][0] = (dpNextState[totalAbsences][0] + dpCurrState[totalAbsences][consecutiveLates]) % MOD;
                     if (totalAbsences < 1) {
-                        dpNextState[totalAbsences + 1][0] = (dpNextState[totalAbsences + 1][0] + dpState[totalAbsences][consecutiveLates]) % MOD;
+                        dpNextState[totalAbsences + 1][0] = (dpNextState[totalAbsences + 1][0] + dpCurrState[totalAbsences][consecutiveLates]) % MOD;
                     }       
                     if (consecutiveLates < 2) {
-                        dpNextState[totalAbsences][consecutiveLates + 1] = (dpNextState[totalAbsences][consecutiveLates + 1] + dpState[totalAbsences][consecutiveLates]) % MOD;
+                        dpNextState[totalAbsences][consecutiveLates + 1] = (dpNextState[totalAbsences][consecutiveLates + 1] + dpCurrState[totalAbsences][consecutiveLates]) % MOD;
                     }
                 }
             }   
-            dpState = dpNextState;
+            for (int i = 0; i < 2; i++) {
+                System.arraycopy(dpNextState[i], 0, dpCurrState[i], 0, 3);
+            }
         }
         int totalCount = 0;
         for (int totalAbsences = 0; totalAbsences < 2; totalAbsences++) {
             for (int consecutiveLates = 0; consecutiveLates < 3; consecutiveLates++) {
-                totalCount = (totalCount + dpState[totalAbsences][consecutiveLates]) % MOD;
+                totalCount = (totalCount + dpCurrState[totalAbsences][consecutiveLates]) % MOD;
             }
         }       
         return totalCount;
