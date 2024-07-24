ee676449269e86f452912506fafa25636f3b3b94
Pranjal Sailwal <127615786+sailwalpranjal@users.noreply.github.com>
2024-07-24 21:25:05 +0530

    Time: 423 ms (25.49%) | Memory: 55.5 MB (59.80%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
@@ -1,21 +1,19 @@
+
 class Solution {
     public int[] sortJumbled(int[] mapping, int[] nums) {
-        // Create an array of indices
         Integer[] indices = new Integer[nums.length];
         for (int i = 0; i < nums.length; i++) {
             indices[i] = i;
         }
 
-        // Sort indices based on the mapped values of nums
         Arrays.sort(indices, (i1, i2) -> {
             long mappedI1 = getMappedValue(nums[i1], mapping);
             long mappedI2 = getMappedValue(nums[i2], mapping);
             return Long.compare(mappedI1, mappedI2);
         });
 
-        // Build the result array based on sorted indices
         int[] result = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < indices.length; i++) {
             result[i] = nums[indices[i]];
         }
 
@@ -24,17 +22,17 @@ class Solution {
 
     private long getMappedValue(int num, int[] mapping) {
         if (num == 0) return mapping[0];
-
+        
         long mappedValue = 0;
         long multiplier = 1;
-
+        
         while (num > 0) {
             int digit = num % 10;
             mappedValue = mapping[digit] * multiplier + mappedValue;
             multiplier *= 10;
             num /= 10;
         }
-
+        
         return mappedValue;
     }
 }
