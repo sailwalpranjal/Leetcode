e524f749206b1ccf8240959770c47165678753bb
Pranjal Sailwal <127615786+sailwalpranjal@users.noreply.github.com>
2024-07-31 20:46:46 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (69.60%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.java
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.java
@@ -1,25 +1,19 @@
 class Solution {
     public int minHeightShelves(int[][] books, int shelfWidth) {
         int n = books.length;
-        int[] dp = new int[n + 1];
-        
-        // Initialize dp array where dp[i] is the minimum height needed for the first i books
-        // Initial condition: no books need no height
+        int[] f = new int[n + 1];
         for (int i = 1; i <= n; ++i) {
-            int width = 0, maxHeight = 0;
-            // Update dp[i] considering placing books from index j to i on the current shelf
-            dp[i] = Integer.MAX_VALUE; // Initialize to a large number
-
-            for (int j = i; j > 0; --j) {
-                width += books[j - 1][0];
-                if (width > shelfWidth) {
-                    break; // Stop if the width exceeds shelfWidth
+            int w = books[i - 1][0], h = books[i - 1][1];
+            f[i] = f[i - 1] + h;
+            for (int j = i - 1; j > 0; --j) {
+                w += books[j - 1][0];
+                if (w > shelfWidth) {
+                    break;
                 }
-                maxHeight = Math.max(maxHeight, books[j - 1][1]); // Update max height of the shelf
-                dp[i] = Math.min(dp[i], dp[j - 1] + maxHeight); // Update minimum height needed
+                h = Math.max(h, books[j - 1][1]);
+                f[i] = Math.min(f[i], f[j - 1] + h);
             }
         }
-        
-        return dp[n]; // Return the minimum height needed for all books
+        return f[n];
     }
 }
